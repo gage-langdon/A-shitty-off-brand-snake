4f1c6b39ef29b5200d9838ea00594ad864f696ab
Gage <gagelangdon@gmail.com>
2020-03-22 15:31:44 -0700

    chore: init scripts

---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+
+    private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        Debug.Log("x: " + transform.position.x);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
