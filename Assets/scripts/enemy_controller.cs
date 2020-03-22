136825455f92501aaa9464a97a9cf490fa706dae
Maxton Delaney <maxtondelaney@gmail.com>
2020-03-22 16:33:24 -0700

    init

--- Assets/scripts/enemy_controller.cs
+++ Assets/scripts/enemy_controller.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class enemy_controller : MonoBehaviour
 {
+    private Transform transform; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+        transform.position = new Vector3(0, 0, 0);  
+
+        transform.position = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));  
     }
 }
