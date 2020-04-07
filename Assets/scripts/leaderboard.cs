0d2c977b290719cbe6b4739dec5bd39eb0c5c874
Gage <gagelangdon@gmail.com>
2020-04-06 22:40:24 -0700

    wip: create leaderboard UI

---
+++ Assets/scripts/leaderboard.cs
@@ -0,0 +1,84 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Shitty.Networking;
+
+public class leaderboard : MonoBehaviour
+{
+
+    public Text leaderboard_text;
+
+    public List<Score> scores = new List<Score>();
+
+
+    public leaderboard()
+    {
+
+    }
+
+    public void Start()
+    {
+        StartCoroutine(getLeaderboard());
+    }
+
+
+    public IEnumerator getLeaderboard()
+    {
+        Http http = new Http();
+        yield return StartCoroutine(http.Get("/scores"));
+        JSONObject scoresJson = new JSONObject(http.data).GetField("scores");
+
+        // reset the leaderboard we have and add the fresh ones
+        scores.Clear();
+        for (int i = 0; i < scoresJson.Count; i++)
+        {
+            string name = scoresJson[i].GetField("name").ToString();
+            name = name.Substring(1, name.Length - 2); // remove quotes ex "gage" => gage
+            int score = int.Parse(scoresJson[i].GetField("score").ToString());
+            Score newScore = new Score(name, score, i);
+            scores.Add(newScore);
+        }
+
+        // Update the UI
+        updateLeaderboardUI();
+
+    }
+
+    void updateLeaderboardUI()
+    {
+        string newLeaderboardText = "";
+
+        // Format and append all the scores to the leaderboard
+        for (int i = 0; i < scores.Count; i++)
+        {
+            newLeaderboardText += scores[i].toString() + "\n";
+        }
+
+        leaderboard_text.text = newLeaderboardText;
+    }
+
+}
+
+public class Score
+{
+    int score;
+    public string name;
+    public int rank;
+
+    public Score(string user_name, int new_score, int new_rank)
+    {
+        name = user_name;
+        score = new_score;
+        rank = new_rank;
+    }
+
+    public string toString()
+    {
+        string formattedString = name;
+        if (formattedString.Length > 10)
+            formattedString = formattedString.Substring(0, 10);
+        return formattedString.PadRight(20, ' ') + " " + score.ToString();
+    }
+}
---
+++ Assets/scripts/network.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+
+namespace Shitty.Networking
+{
+    public class Http : MonoBehaviour
+    {
+
+        public string data = "";
+        public bool error = false;
+
+        public string apiURI = "https://1nmquhg2il.execute-api.us-west-2.amazonaws.com/prod/snek";
+
+        public IEnumerator Post(string url, string bodyJsonString)
+        {
+            var request = new UnityWebRequest(apiURI + url, "POST");
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            Debug.Log("Status Code: " + request.responseCode);
+        }
+
+
+        public IEnumerator Get(string url)
+        {
+            var http = new UnityWebRequest(apiURI + url, "GET");
+            http.SetRequestHeader("Content-Type", "application/json");
+            http.downloadHandler = new DownloadHandlerBuffer();
+            yield return http.SendWebRequest();
+
+            if (http.error != null)
+            {
+                error = true;
+                yield return false;
+            };
+            data = http.downloadHandler.text;
+
+            // Debug.Log(http.responseCode);
+            // Debug.Log(http.error);
+            // Debug.Log(http.downloadHandler.text);
+        }
+    }
+}
+
