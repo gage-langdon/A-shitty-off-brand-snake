0cabb6c0eae29bf44cffd1f62a88f93e2943bbbe
Gage <glangdon@loanpal.com>
2020-04-08 00:33:21 -0700

    fix: add cors headers to response

--- backend/handlers/scores.js
+++ backend/handlers/scores.js
@@ -1,43 +1,37 @@
 const UserModel = require("../models/user");
+const { Response } = require("../utils/response");
 
-module.exports.getScores = async () => {
-  const User = await UserModel();
-  const users = await User.getUsersSortedByScore(10);
-  const leaderboard = users.map(({ name, score }, i) => ({
-    name,
-    score,
-    rank: i + 1,
-  }));
+module.exports.getScores = async (event, context, callback) => {
+  const res = Response(callback);
+  try {
+    const User = await UserModel();
+    const users = await User.getUsersSortedByScore(10);
+    const scores = users.map(({ name, score }, i) => ({
+      name,
+      score,
+      rank: i + 1,
+    }));
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        scores: leaderboard,
-      },
-      null,
-      2
-    ),
-  };
+    res.send({
+      scores,
+    });
+  } catch (e) {
+    console.error("Failed to get scores. Error: ", e);
+    res.error({ message: "Failed to get scores." });
+  }
 };
 
 module.exports.postScore = async (event) => {
+  const res = Response(callback);
   try {
     const { score, name } = JSON.parse(event.body);
 
     const User = await UserModel();
-    const newUser = User.create({ name, score });
+    const newUser = await User.create({ name, score });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(newUser, null, 2),
-    };
+    res.send({ ok: true });
   } catch (e) {
-    console.log("Failed to post score. Error: ", e);
-
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Failed to post score." }, null, 2),
-    };
+    console.error("Failed to post score. Error: ", e);
+    res.error({ message: "Failed to post score." });
   }
 };
